f913eb13bbf8f8c940fe31fed02e76dca07257eb
IlyaZolotarev <zolotarevgroup@gmail.com>
2020-05-10 22:22:35 +0300

    Updates

---
+++ precache-manifest.54f75c9cd1e2491eacd31501274331a3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea82bb3aefff75f2b4c389e9ed09782",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "03865da19834a0712a8a",
+    "url": "/todo/static/css/main.6984b7c7.chunk.css"
+  },
+  {
+    "revision": "4117dbde2ec6fa732c9c",
+    "url": "/todo/static/js/2.2c7ba3df.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/todo/static/js/2.2c7ba3df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03865da19834a0712a8a",
+    "url": "/todo/static/js/main.34c526ba.chunk.js"
+  },
+  {
+    "revision": "7a49d9b4c98a10277257",
+    "url": "/todo/static/js/runtime-main.d3ef14df.js"
+  },
+  {
+    "revision": "6d35d8e403413bd0c3d1c256a9bc73fe",
+    "url": "/todo/static/media/Alwyn-NewRounded-Regular.6d35d8e4.ttf"
+  },
+  {
+    "revision": "be82e3850974c1ccddfcf305d93ad7ce",
+    "url": "/todo/static/media/MyriadPro-Regular.be82e385.ttf"
+  },
+  {
+    "revision": "5a87cce84010f7cea085ae218d44a64b",
+    "url": "/todo/static/media/NotoSans-Regular.5a87cce8.ttf"
+  },
+  {
+    "revision": "b99bd621ca00a42e7edb31c3bfc4eeb7",
+    "url": "/todo/static/media/avatar.b99bd621.jpg"
+  },
+  {
+    "revision": "33f225b8f5f7d6b34a0926f58f96c1e9",
+    "url": "/todo/static/media/opensans-semibold.33f225b8.ttf"
+  },
+  {
+    "revision": "91505eeeb58a87e4b253c43157d3581f",
+    "url": "/todo/static/media/search.91505eee.svg"
+  },
+  {
+    "revision": "0099ec3754bf473d2bbf317204ab6fea",
+    "url": "/todo/static/media/trello-logo.0099ec37.png"
+  }
+]);
